c98fbde2d5cb452799484aae5b9cb71f144c9851
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-06 12:20:25 -0500

    created store, added imports to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App/App";
+import { createStore, applyMiddleware, compose } from "redux";
+import { userReducer } from "./Redux/Reducers/userReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(userReducer, composeEnhancer(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
