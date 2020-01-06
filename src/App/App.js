77915a08fd9343021eab2371a937ac680db202d9
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-06 12:02:13 -0500

    set up skeleton for website

--- src/App/App.js
+++ src/App/App.js
@@ -12,7 +12,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Fetching Data</h1>
+
+        <form className="App-form" onSubmit>
+          <label>Search GitHub Users</label>
+          <input
+            className="App-input-text"
+            type="text"
+            placeholder="User Name"
+          />
+          <input className="App-button" type="submit" placeholder="Search" />
+        </form>
+
+        <div className="App-user-info-box">
+          <h2>User Info Goes Here</h2>
+        </div>
+      </header>
     </div>
   );
 }
