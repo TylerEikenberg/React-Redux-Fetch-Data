41f9481c09b0e81bc7913b573855637bc7497d2b
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-06 11:54:23 -0500

    updated readme

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./App.css";
+
+/**
+ * Use github api
+ * Have form to type username
+ * use username value to fetch to github api to fetch user data
+ * display data
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
